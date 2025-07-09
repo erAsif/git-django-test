8c97682422cea03228a6df18d72a39cf822b02f7
erasif <erasif3210@gmail.com>
2025-07-09 14:22:41 +0530

    db change

--- gitt/views.py
+++ gitt/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+# default_db = env.db(
+    # default="postgres://postgres:asif@7275@localhost:5432/clocksession-dev")
+
+
+#     for db_dict in [default_db]:
+#     if not db_dict.get("HOST", None) and not db_dict.get("host", None):
+#         db_dict = parse_db_host(db_dict)
+
+# # Database
+# # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+# default_db['ENGINE'] = 'django_db_geventpool.backends.postgresql_psycopg2'
+# default_db['CONN_MAX_AGE'] = 0
+# default_db['OPTIONS'] = {
+#             'MAX_CONNS': 30,
+#             'REUSE_CONNS': 5
+#         }
+
+
+# DATABASES = {"default": default_db}
+
